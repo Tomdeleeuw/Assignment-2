bca7366a4193a9adea3b147191904f36cd1543ea
Tomdeleeuw <tomdeleeuw1@outlook.com>
2022-10-07 13:59:34 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c2933b599731a74c83cf3ba20aabdda3c8877341670c77cbc5f89acd47f97671
+size 2390
---
+++ utils.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d003f9d30e8c27c4f51f5a11dc5141af4230583a8415262f49e2ed50f817587e
+size 971
